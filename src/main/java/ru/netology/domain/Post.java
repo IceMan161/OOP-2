b218021b22671e0444116ec16cbb1567d300ecea
Artem Sukhanov <IceMan161@yandex.ru>
2021-12-24 23:23:44 +0300

    VK

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,10 +1,31 @@
 package ru.netology.domain;
 
 public class Post {
-    private String id;
-    private String postUrl;
-    private String date;
+    private int id;
+    private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int date;
     private String text;
-    private String post;
-    private String info;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private String postType;
+    private int signerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private int postponedId;
+    private CommentsInfo commentsInfo;
+    private DonutInfo donutInfo;
+    private GeoInfo geoInfo;
+    private LikesInfo likesInfo;
+    private PlaceholderInfo placeholderInfo;
+    private PlaceInfo placeInfo;
+    private PostSource postSource;
+    private RepostsInfo repostsInfo;
+    private ViewsInfo viewsInfo;
 }

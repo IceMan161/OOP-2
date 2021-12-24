0926c77abfbb186848e5e9ae0046410067e438a9
Artem Sukhanov <IceMan161@yandex.ru>
2021-12-24 21:21:51 +0300

    Initial Commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class Post {
+    private String id;
+    private String postUrl;
+    private String date;
+    private String text;
+    private String post;
+    private String info;
+}
